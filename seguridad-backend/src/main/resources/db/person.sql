3b8b8f2927c5d53c7a595749562064ace5c4b7ec
freddyheredia4 <freddyheredia4@gmail.com>
2022-08-04 16:09:55 -0500

    add db person

--- seguridad-backend/src/main/resources/db/person.sql
+++ seguridad-backend/src/main/resources/db/person.sql
@@ -2,6 +2,13 @@
 
 -- DROP TABLE IF EXISTS public.persons;
 
+CREATE SEQUENCE IF NOT EXISTS public.persons_person_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 2147483647
+    CACHE 1;
+
 CREATE TABLE IF NOT EXISTS public.persons
 (
     person_id integer NOT NULL DEFAULT nextval('persons_person_id_seq'::regclass),
@@ -10,10 +17,11 @@ CREATE TABLE IF NOT EXISTS public.persons
     created timestamp without time zone,
     enabled boolean,
     updated timestamp without time zone,
-    CONSTRAINT persons_pkey PRIMARY KEY (person_id)
+    city_id integer,
+    CONSTRAINT persons_pkey PRIMARY KEY (person_id),
+    CONSTRAINT fk_city FOREIGN KEY (city_id)
+        REFERENCES public.city (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
 )
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.persons
-    OWNER to postgres;
